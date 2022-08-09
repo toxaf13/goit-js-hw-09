075c9bf3e475560062f073383cf7a6c9447d17b0
wombatstar <toxaf13@gmail.com>
2022-08-09 17:49:17 +0200

    timer=null

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,9 +2,12 @@ const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 
+let timer = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
+
 
 btnStart.addEventListener('click',() => {
    btnStart.disabled = true;

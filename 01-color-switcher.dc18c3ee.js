6ab93b1fe5af42b8e699298d069c1ff22cca9930
toxaf13 <toxaf13@gmail.com>
2022-08-09 15:49:45 +0000

    Deploying to gh-pages from @ toxaf13/goit-js-hw-09@075c9bf3e475560062f073383cf7a6c9447d17b0 🚀

---
+++ 01-color-switcher.dc18c3ee.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.dc18c3ee.js.map
